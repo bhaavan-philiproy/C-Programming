4ce710dd144a51fb74a7d589a6d060d6da3b198e
Bhaavan Philip Roy <96874838+bhaavan-philiproy@users.noreply.github.com>
2021-12-30 20:20:33 +0530

    Create Krishnamurthy_number.c

---
+++ PC/Krishnamurthy_number.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<conio.h>
+int fact(int);
+void main()
+{
+    int n,m,f,sum=0;
+    printf("Enter the number : ");
+    scanf("%d",&n);
+    while(n!=0)
+    {
+        m=n%10;
+        f=fact(m);
+        sum=sum+f;
+        n=n/10;
+    }
+    printf("%d\n",sum);
+    if(sum==n)
+    {
+        printf("The number is a Krishnamurthy Number");
+    }
+    else
+    {
+    printf("The number is not a Krishnamurthy Number");
+    }
+    getch();
+}
+int fact(int m)
+{
+    if(m==0)
+    
+    return 1;
+    else if(m==1)
+    return 1;
+    else 
+    return m*fact(m-1);
+}
