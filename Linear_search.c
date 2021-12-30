9134ff5a773ff58ab27650e9b00883627d4b8bee
Bhaavan Philip Roy <96874838+bhaavan-philiproy@users.noreply.github.com>
2021-12-30 20:13:01 +0530

    Create Linear_search.c

---
+++ Linear_search.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int a[100],b,n,i;
+    printf("Enter limit of array");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",a[i]);
+        
+    }
+    printf("Enter the no. to be searched");
+    scanf("%d",&b);
+    for(i=0;i<n;i++)
+    {
+        if(a[i]==b)
+        {
+            printf("Element found at %d position",i+1);
+            break;
+        }
+    }
+    if(i==n)
+    printf("The no. is not present in the array");
+    return 0;
+}
