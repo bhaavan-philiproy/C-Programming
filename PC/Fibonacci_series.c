1e4efa09082de985932de4bd56ba17c6f7f990a7
Bhaavan Philip Roy <96874838+bhaavan-philiproy@users.noreply.github.com>
2021-12-30 20:17:30 +0530

    Create Fibonacci_series.c

---
+++ PC/Fibonacci_series.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a=0,b=1,c,i,n;
+    printf("Enter the number of elements :\n");
+    scanf("%d",&n);
+    printf("\n %d %d ",a,b);
+    for(i=2;i<n;i++)
+    {
+        c=a+b;
+        printf("%d ",c);
+        a=b;
+        b=c;
+    }
+    getch();
+}
