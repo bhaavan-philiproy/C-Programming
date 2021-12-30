5a6054bf82a834be3b05ded79400ab4d63183c12
Bhaavan Philip Roy <96874838+bhaavan-philiproy@users.noreply.github.com>
2021-12-30 20:22:00 +0530

    Create ASCII_value.c

---
+++ PC/ASCII_value.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char c;
+    printf("Enter a single character : ");
+    scanf("%c",&c);
+    printf("The ASCII value of %c is %d",c,c);
+    getch();
+}
