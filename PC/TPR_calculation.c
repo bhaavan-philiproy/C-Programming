3469293250e7fecd9b525f72ab04af894722e57e
Bhaavan Philip Roy <96874838+bhaavan-philiproy@users.noreply.github.com>
2021-12-30 20:19:39 +0530

    Create TPR_calculation.c

---
+++ PC/TPR_calculation.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    float tpr,t,p;
+    printf("Enter the total number of samples : ");
+    scanf("%f",&t);
+    printf("Enter the number of positive cases : ");
+    scanf("%f",&p);
+    tpr=p*100/t;
+    printf("TPR = %f",tpr);
+    if(tpr<=5)
+    {
+        printf("\nThe condition is under control");
+    }
+    else
+    {
+        printf("\nThe condition is not under control");
+    }
+    getch();
+}
