9386a6e8e5c12d1174c6d2dacde06df0dd95f7a2
Bhaavan Philip Roy <96874838+bhaavan-philiproy@users.noreply.github.com>
2021-12-30 20:16:17 +0530

    Create Euclidean_space.c

---
+++ PC/Euclidean_space.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void
+main ()
+{
+  struct dis
+  {
+    int a;
+    int b;
+  } val;
+  printf ("Enter the distanced between first point and reference point\n");
+  scanf ("%d", &val.a);
+  printf ("Enter the distance between second point ant reference point\n");
+  scanf ("%d", &val.b);
+  printf ("Total distance=%d", val.a + val.b);
+  getch ();
+}
